f697adb46d6cc3b81b62bffe78bd4ff0d3c16ded
Peter Vlugter <pvlugter@gmail.com>
2013-06-08 12:57:15 -0700

    Add atmos:run-main task

--- src/main/scala/com/typesafe/sbt/SbtAtmos.scala
+++ src/main/scala/com/typesafe/sbt/SbtAtmos.scala
@@ -122,7 +122,8 @@ object SbtAtmos extends Plugin {
     ) map AtmosInputs,
 
     inScope(Scope(This, Select(Compile), Select(run.key), This))(Seq(runner in run in Atmos <<= atmosRunner)).head,
-    run <<= Defaults.runTask(fullClasspath in Runtime, mainClass in run in Compile, runner in run)
+    run <<= Defaults.runTask(fullClasspath in Runtime, mainClass in run in Compile, runner in run),
+    runMain <<= Defaults.runMainTask(fullClasspath in Runtime, runner in run)
   )
 
   def atmosUnscopedSettings: Seq[Setting[_]] = Seq(
