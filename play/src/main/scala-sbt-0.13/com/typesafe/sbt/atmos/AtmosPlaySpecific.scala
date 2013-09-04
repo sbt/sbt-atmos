612dd7c45dbd017479726ae02b8c8909ac7c6cda
Peter Vlugter <pvlugter@gmail.com>
2013-09-04 13:11:12 +1200

    Override atmos:run with atmos-play:run for play 2.2 plugin

--- play/src/main/scala-sbt-0.13/com/typesafe/sbt/atmos/AtmosPlaySpecific.scala
+++ play/src/main/scala-sbt-0.13/com/typesafe/sbt/atmos/AtmosPlaySpecific.scala
@@ -10,12 +10,14 @@ import play.Keys.playRunHooks
 import play.Project.{ playRunTask, playReloaderClasspath, playReloaderClassLoader }
 
 object AtmosPlaySpecific {
+	import SbtAtmos.Atmos
 	import SbtAtmosPlay.AtmosPlay
   import SbtAtmosPlay.AtmosPlayKeys.weavingClassLoader
 
   def atmosPlaySpecificSettings(): Seq[Setting[_]] = Seq(
     playRunHooks in AtmosPlay <<= playRunHooks,
     playRunHooks in AtmosPlay <+= AtmosPlayRun.createRunHook,
-    run in AtmosPlay <<= playRunTask(playRunHooks in AtmosPlay, externalDependencyClasspath in AtmosPlay, weavingClassLoader in AtmosPlay, playReloaderClasspath, playReloaderClassLoader)
+    run in AtmosPlay <<= playRunTask(playRunHooks in AtmosPlay, externalDependencyClasspath in AtmosPlay, weavingClassLoader in AtmosPlay, playReloaderClasspath, playReloaderClassLoader),
+    run in Atmos <<= run in AtmosPlay
   )
 }
