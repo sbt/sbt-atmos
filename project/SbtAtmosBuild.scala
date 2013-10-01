b8c6e28f3fec5baf1700dd2d3bc8b7c026984a1a
Peter Vlugter <pvlugter@gmail.com>
2013-10-01 18:09:56 +0200

    Version 0.3.1

--- project/SbtAtmosBuild.scala
+++ project/SbtAtmosBuild.scala
@@ -3,7 +3,7 @@ import sbt.Keys._
 import net.virtualvoid.sbt.cross.CrossPlugin
 
 object SbtAtmosBuild extends Build {
-  val Version = "0.3.1-SNAPSHOT"
+  val Version = "0.3.1"
 
   lazy val sbtAtmos = Project(
     id = "sbt-atmos",
